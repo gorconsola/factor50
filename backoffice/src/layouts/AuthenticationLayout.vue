cac0d132d592ea42c626411844313a5020aefb08
gorconsola <c.y.kroon@gmail.com>
2020-06-09 21:21:29 +0200

    Add Bacckoffice

---
+++ backoffice/src/layouts/AuthenticationLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="authentication">
+    <div class="columns is=mobile no-margin">
+      <div class="column is-half is-offset-one-quarter">
+        <section class="section section--black">
+          <h1>SPF50</h1>
+
+          <slot name="header">
+            <h3>{{ title }}</h3>
+          </slot>
+
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthenticationLayout',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ backoffice/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <authentication-layout title="Log in">
+
+  </authentication-layout>
+</template>
+
+<script>
+import AuthenticationLayout from '@/layouts/AuthenticationLayout'
+
+export default {
+  name: 'Login',
+  components: {
+    AuthenticationLayout
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
