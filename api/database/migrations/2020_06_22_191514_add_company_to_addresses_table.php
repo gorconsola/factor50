f879365594617308b17bf185ffdb6421dac3019d
gorconsola <c.y.kroon@gmail.com>
2020-06-22 21:20:37 +0200

    Add company to addresses table migration

--- api/database/migrations/2020_06_22_191514_add_company_to_addresses_table.php
+++ api/database/migrations/2020_06_22_191514_add_company_to_addresses_table.php
@@ -14,7 +14,7 @@ class AddCompanyToAddressesTable extends Migration
     public function up()
     {
         Schema::table('addresses', function (Blueprint $table) {
-            //
+            $table->string('company')->after('last_name')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddCompanyToAddressesTable extends Migration
     public function down()
     {
         Schema::table('addresses', function (Blueprint $table) {
-            //
+            $table->dropColumn('company');
         });
     }
 }
